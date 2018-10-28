34dd4dc881bd870b8f63537e536ee8ec14aca4f6
Andrei Vasilev <you@example.com>
2018-10-28 17:03:36 +0200

    added register mutation

---
+++ src/index.ts
@@ -0,0 +1,19 @@
+import "reflect-metadata";
+
+
+import { GraphQLServer } from 'graphql-yoga'
+
+import { importSchema } from 'graphql-import'
+import { resolvers } from './resolvers'
+
+import * as path from 'path'
+import { createConnections } from "typeorm";
+
+const typeDefs = importSchema(path.join(__dirname, './schema.graphql'))
+
+
+
+const server = new GraphQLServer({ typeDefs, resolvers })
+createConnections().then(() => {
+  server.start(() => console.log('Server is running on localhost:4000'))
+})
